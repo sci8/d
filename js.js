45478fce94ec910357c9320df89c22e445655ab8
sci8 <sci8ic@gmail.com>
2022-07-09 21:44:26 +0900

    data-date-toISOString

--- js.js
+++ js.js
@@ -1,2 +1,2 @@
 'use strict';
-addEventListener('DOMContentLoaded', e => ['data-time-stamp'].forEach(attr => [...document.body.querySelectorAll(`[${attr}]`)].forEach(elm => elm.setAttribute(attr, new Date(Number(elm.getAttribute(attr).value)).toISOString()))));
+addEventListener('DOMContentLoaded', e => ['data-time-stamp'].forEach(attr => [...document.body.querySelectorAll(`[${attr}]`)].forEach(elm => elm.setAttribute('data-date-toISOString', new Date(Number(elm.getAttribute(attr).value)).toISOString()))));
