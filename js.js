9fac25de5763b272f7c5a755d980356c0bd78cbb
sci8 <sci8ic@gmail.com>
2022-07-09 17:57:19 +0900

    test

--- js.js
+++ js.js
@@ -1,1 +1,2 @@
 'use strict';
+addEventListener('DOMContentLoaded', e => ['data-time-stamp'].forEach(attr => [...document.body.querySelectorAll(`[${attr}]`)].forEach(elm => elm.setAttribute(attr, new Date(Number(elm.getAttribute(attr).value)).toISOString()))));
