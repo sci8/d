00070bc96f13f491eb0a0067fdd1b0a0e916028e
sci8 <sci8@users.noreply.github.com>
2022-07-11 00:34:38 +0900

    Update js.js

--- js.js
+++ js.js
@@ -3,7 +3,7 @@ new Promise(addEventListener.bind(this, 'DOMContentLoaded'))
 .then(e => {
     const a_css = 'data-Date_toISOString';
     [...document.body.querySelectorAll('[id^="ID_"]')].forEach(s => s.setAttribute(a_css, new Date(Number(s.id.match(/\d+$/))).toISOString()));
-    document.head.appendChild(document.createElement('style')).sheet.insertRule(`[${a_css}]::before {content: "attr(${a_css})"; display: block;}`, 0);
+    document.head.appendChild(document.createElement('style')).sheet.insertRule(`[${a_css}]::before {content: attr(${a_css}); display: block;}`, 0);
     return e;
 })
 .catch(() => undefined);
