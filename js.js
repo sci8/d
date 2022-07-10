8853914f0adb800721965262eaf32147fc8eefde
sci8 <sci8@users.noreply.github.com>
2022-07-11 00:31:30 +0900

    Update js.js

--- js.js
+++ js.js
@@ -1,10 +1,9 @@
 'use strict';
 new Promise(addEventListener.bind(this, 'DOMContentLoaded'))
 .then(e => {
-    const a_html = 'data-Date_getTime';
     const a_css = 'data-Date_toISOString';
-    document.head.appendChild(document.createElement('style')).sheet.insertRule(`[${a_css}]::before {content: attr(${a_css}); display: block;}`, 0);
-    [...document.body.querySelectorAll(`[${a_html}]`)].forEach(s => s.setAttribute(a_css, new Date(Number(s.getAttribute(a_html))).toISOString()));
+    [...document.body.querySelectorAll('[id^="ID_"]')].forEach(s => s.setAttribute(a_css, new Date(Number(s.id.match(/\d+$/))).toISOString()));
+    document.head.appendChild(document.createElement('style')).sheet.insertRule(`[${a_css}]::before {content: "attr(${a_css})"; display: block;}`, 0);
     return e;
 })
 .catch(() => undefined);
