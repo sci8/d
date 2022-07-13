82f46eaf34d15737e0b4baaece5d2b3bcd0cc8df
sci8 <sci8@users.noreply.github.com>
2022-07-13 13:00:01 +0900

    Update js.js

--- js.js
+++ js.js
@@ -1,16 +1,16 @@
 'use strict';
 new Promise(addEventListener.bind(this, 'DOMContentLoaded'))
 .then(e => {
-    const id_prefix = 'ID_TIME_STAMP_';console.log(id_prefix);
-    for (const article of document.body.querySelectorAll(`[id^="${id_prefix}"]`)) {
+    const css_selector = '[id^="ID_TIME_STAMP_"]';
+    for (const article of document.body.querySelectorAll(css_selector)) {
         const time = article.insertBefore(document.createElement('section'), article.firstElementChild).appendChild(document.createElement('p')).appendChild(document.createElement('time'));
         time.setAttribute('datetime', time.appendChild(document.createTextNode(new Date(Number(article.id.match(/\d+$/))).toISOString())).wholeText);
     }
-    return {e, id_prefix};
+    return {e, css_selector};
 })
-.then(({e, id_prefix}) => {
-    // document.head.appendChild(document.createElement('style')).sheet.insertRule(`[${a_css}]::before {content: attr(${a_css}); display: block;}`, 0);
+.then(({e, css_selector}) => {
+    const {sheet} = document.head.appendChild(document.createElement('style'));
+    sheet.insertRule(`${css_selector} {display: flex;}`, 0);
     return e;
 })
-// .catch(() => undefined);
-;
+.catch(() => undefined);
